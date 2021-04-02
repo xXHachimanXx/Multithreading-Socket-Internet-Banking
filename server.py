f1dc3d35fa89c1394023322b5c32440e5022bbff
xXHachimanXx <globaldep90@gmail.com>
2021-04-02 14:56:57 -0300

    Correcting syntactic error in json.loads.

--- server.py
+++ server.py
@@ -8,87 +8,83 @@ from services.AccountService import AccountService
 import multiprocessing
 import queue
 import json
+import socket
 
 HOST = '127.0.0.1'
-PORT = 65432
+PORT = 3333
 database = AccountDatabase()
 queues = []
 
 class DataReceiver(Thread):
-    def __init__(self, socket):
-        self.socket = socket
-        super().__init__()
+   def __init__(self, socket):
+      self.socket = socket
+      super().__init__()
 
-    def run(self) -> None:
-        while True:
-            # establish connection with client
-            conn, addr = self.socket.accept()
+   def run(self) -> None:
+      while True:
+         # establish connection with client
+         conn, addr = self.socket.accept()
 
-            with conn:
-                # lock acquired by client
-                print('Connected by', addr)
-                alldata = bytes()
+         with conn:
+               # lock acquired by client
+               print('Connected by', addr)
+               alldata = bytes()
+               while True:
+                  data = conn.recv(1024)
+                  if not data:
+                     break
+                  alldata += data
+               
+               print('Received #', threading.get_ident(), repr(alldata))
+               
+               # client data to string
+               operation = json.loads(alldata.decode('utf-8'))
+               # sendToQueue(operation)
+               account_number = operation["account_number"]
+               operation_type = operation["type"]
+               value = operation["value"]
 
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    alldata += data
-                
-                print('Received #', threading.get_ident(), repr(alldata))
-                
-                # client data to string
-                operation = loads(str(alldata.decode('utf-8'))
-                sendToQueue(operation)
+               index = account_number % len(queues)
+               t = AccountService(target=func, args=((value, account_number, operation_type, queues[index])))
+               t.start()
+               
+               conn.sendall(alldata)
+   
+   def do_operation(value: int, account_number: int, operation_type: str, q: queue.Queue):
+      operation = {
+         'account_number': account_number,
+         'type': operation_type,
+         'value': value
+      }
 
-                #conn.sendall(alldata)
-    def sendToQueue(operation: object):
+   def response(value: object):
+      print(operation)
+      print('result', value)
+      print('-----------------------')
+      q.put((operation, response)) 
 
-        account_number = operation["account_number"]
-        operation_type = operation["type"]
-        value = operation["value"]
-
-        index = account_number % len(queues)
-        t = AccountService(target=func, args=((value, account_number, operation_type, queues[index])))
-        t.start()
-
-    def do_operation(value: int, account_number: int, operation_type: str, q: queue.Queue):
-        operation = {
-            'account_number': account_number,
-            'type': operation_type,
-            'value': value
-        }
-
-    def response(value: object):
-        print(operation)
-        print('result', value)
-        print('-----------------------')
-        q.put((operation, response)) 
-
-    # def run(func, operation_type, value, account_number):
-    #     index = account_number % len(queues)
-    #     t = AccountService(target=func, args=((value, account_number, operation_type, queues[index])))
-    #     t.start()
-
+# def run(func, operation_type, value, account_number):
+#     index = account_number % len(queues)
+#     t = AccountService(target=func, args=((value, account_number, operation_type, queues[index])))
+#     t.start()
 
 
 def Main():
 
-    # create queues, AccoutServices and database
-    for i in range(multiprocessing.cpu_count()):
-        queues.append(queue.Queue()) 
-        AccountService(queues[i], database).start()
+   # create queues, AccoutServices and database
+   for i in range(multiprocessing.cpu_count()):
+      queues.append(queue.Queue()) 
+      AccountService(queues[i], database).start()
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        print(f'Listening on {HOST}:{PORT}')
+   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      s.bind((HOST, PORT))
+      s.listen()
+      print(f'Listening on {HOST}:{PORT}')
 
-        for i in range(multiprocessing.cpu_count()):
-            DataReceiver(s).start()
+      for i in range(multiprocessing.cpu_count()):
+         DataReceiver(s).start()
 
-if __name__ == '__main__':
-    Main()
+Main()
 
 
 # run(do_operation, 'deposit', 100, 0)

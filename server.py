77ab698e6e7c919d4c78dc142ff8a31f3994d347
xXHachimanXx <globaldep90@gmail.com>
2021-04-03 16:36:14 -0300

    Refactoring DataReceiver class.

--- server.py
+++ server.py
@@ -22,52 +22,46 @@ class DataReceiver(Thread):
       super().__init__()
 
    def run(self) -> None:
-         with conn:
-               # lock acquired by client
-               print('Connected by', addr)
-               alldata = bytes()
-               while True:
-                  data = conn.recv(1024)
-                  if not data:
-                     break
-                  alldata += data
-               
-               print('Received #', threading.get_ident(), repr(alldata))
-               
-               # client data to string
-               operation = json.loads(alldata.decode('utf-8'))
-               sendToQueue(operation)
-               # conn.sendall(alldata)
+      with self.conn:
+         # lock acquired by client
+         print('Connected by', self.addr)
+         alldata = bytes()
+         while True:
+            data = self.conn.recv(1024)
+            if not data:
+               break
+            alldata += data
+         
+         print('Received #', threading.get_ident(), repr(alldata))
+         
+         # client data to string
+         operation = json.loads(alldata.decode('utf-8'))
+         send_to_queue(operation)
+         # conn.sendall(alldata)
 
-   def sendToQueue(operation: object):
+   def send_to_queue(operation: object):
       account_number = operation["account_number"]
       operation_type = operation["type"]
       value = operation["value"]
 
+      def response(value: object):
+         print(operation)
+         print('result', value)
+         print('-----------------------')
+         # send_to_client
+
       index = account_number % len(queues)
-      t = AccountService(target=func, args=((value, account_number, operation_type, queues[index])))
-      t.start()
-
+      queues[index].put((operation, response))
    
-   def do_operation(value: int, account_number: int, operation_type: str, q: queue.Queue):
-      operation = {
-         'account_number': account_number,
-         'type': operation_type,
-         'value': value
-      }
-
-   def response(value: object):
-      print(operation)
-      print('result', value)
-      print('-----------------------')
-      q.put((operation, response)) 
-
 # def run(func, operation_type, value, account_number):
 #     index = account_number % len(queues)
 #     t = AccountService(target=func, args=((value, account_number, operation_type, queues[index])))
 #     t.start()
 
-
+def send_to_some_data_receiver(conn, addr):
+   t = DataReceiver(conn, addr)
+   t.start()
+   
 def Main():
 
    # create queues, AccoutServices and database
@@ -83,9 +77,7 @@ def Main():
       while True:
          # establish connection with client
          conn, addr = s.accept()
-
-      for i in range(multiprocessing.cpu_count()):
-         DataReceiver(conn, addr).start()
+         send_to_some_data_receiver(conn, addr)
 
 Main()
 
